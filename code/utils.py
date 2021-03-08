86792c8ef763cabe0f1eb3d7648f5777b1d59122
Elliot Wilens <wilensel@gmail.com>
2021-03-07 21:00:32 -0600

    Add functionality to utils involving cxl model evaluation

--- code/utils.py
+++ code/utils.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+
 from collections import defaultdict
 import datetime
 from dateutil.relativedelta import *
@@ -325,3 +329,111 @@ def split_reservations(df_res, as_of_date, features, y_col="IsCanceled"):
     print(f"Training sample size: {len(X_train)} \n Testing Sample Size: {len(X_test)}")
 
     return X_train, X_test, y_train, y_test
+
+
+def make_confusion_matrix(
+    y_test,
+    y_predict,
+    label_color="black",
+    save_to=False,
+    threshold=0.5,
+    title=None,
+    facecolor="#5c5c5c",
+):
+    # Predict class 1 if probability of being in class 1 is greater than threshold
+    # (model.predict(X_test) does this automatically with a threshold of 0.5)
+
+    confusion = confusion_matrix(y_test, y_predict)
+
+    fig, ax = plt.subplots(dpi=165, figsize=(7, 5))
+    sns.set(font_scale=1.3)
+    group_counts = ["{0:0.0f}".format(value) for value in confusion.flatten()]
+    group_percentages = [
+        "{0:.2%}".format(value) for value in confusion.flatten() / np.sum(confusion)
+    ]
+    labels = [f"{v2}\n\n{v3}" for v2, v3 in zip(group_counts, group_percentages)]
+    labels = np.asarray(labels).reshape(2, 2)
+
+    label_font = {
+        "family": "Arial",
+        "color": label_color,
+        "weight": "bold",
+        "size": 17,
+    }
+
+    title_font = {
+        "family": "Arial",
+        "color": label_color,
+        "weight": "bold",
+        "size": 27,
+    }
+
+    sns.heatmap(
+        confusion,
+        annot=labels,
+        fmt="",
+        cmap="Blues",
+        cbar=False,
+        xticklabels=["will_come", "will_cancel"],
+        yticklabels=["will_come", "will_cancel"],
+    )
+
+    if title == None:
+        title = "Confusion Matrix"
+    plt.title(title, fontdict=label_font)
+    plt.xlabel("Prediction", fontdict=label_font)
+    plt.ylabel("Actual", fontdict=label_font)
+    if save_to:
+        plt.tight_layout()
+        plt.savefig(
+            save_to, dpi=170, facecolor=facecolor, bbox_inches="tight", pad_inches=1.6
+        )
+
+
+def get_preds(pred_probas, threshold, y_test):
+    df_preds = pd.DataFrame(pred_probas, columns=["no_cxl_proba", "cxl_proba"])
+    df_preds["prediction"] = df_preds["cxl_proba"] >= threshold
+    df_preds["actual"] = y_test.to_numpy()
+    return df_preds
+
+
+def get_fbeta_score(pred_probas, beta, threshold, y_test):
+    df_preds = get_preds(pred_probas, threshold, y_test)
+    precision = precision_score(y_test, df_preds["prediction"])
+    recall = recall_score(y_test, df_preds["prediction"])
+    fbeta_score = ((1 + beta ** 2) * precision * recall) / (
+        beta ** 2 * precision + recall
+    )
+    return round(fbeta_score, 3)
+
+
+def optimize_prob_threshold(
+    model, X_test, y_test, beta=0.5, thresh_start=0.4, thresh_stop=0.95
+):
+
+    pred_probas = model.predict_proba(X_test)
+
+    thresholds = np.arange(thresh_start, thresh_stop, 0.01)
+    fbetas = {}  # will hold {prob_thresh: resulting_fbeta_score}
+
+    for t_val in thresholds:
+        fbetas[t_val] = get_fbeta_score(pred_probas, beta, t_val, y_test)
+
+    best_thresh = 0
+    best_fbeta = 0
+
+    for threshold, fb_score in fbetas.items():
+        if fb_score > best_fbeta:
+            best_thresh = threshold
+            best_fbeta = fb_score
+        else:
+            continue
+
+    df_preds = get_preds(pred_probas, best_thresh, y_test)
+    make_confusion_matrix(df_preds.actual, df_preds.prediction, threshold=best_thresh)
+
+    print(
+        f"Optimal probability threshold (to maximize F-{beta}): {best_thresh} \n F-{beta} Score: {best_fbeta}"
+    )
+
+    return best_thresh, best_fbeta
