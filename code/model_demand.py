9b3c6f43a5d931ca82707ee284b93691e47e31aa
Elliot Wilens <wilensel@gmail.com>
2021-03-08 21:08:55 -0600

    Update model_demand with add_sim_cols func, start add_stly

--- code/model_demand.py
+++ code/model_demand.py
@@ -1,4 +1,11 @@
-def setup_sim(df_res, as_of_date="2017-08-01"):
+import pandas as pd
+import numpy as np
+import datetime
+from dateutil.relativedelta import *
+from collections import defaultdict
+
+
+def setup_sim(df_futures, as_of_date="2017-08-01"):
     """
     Takes reservations and returns a DataFrame that can be used as a revenue management simulation.
 
@@ -10,12 +17,12 @@ def setup_sim(df_res, as_of_date="2017-08-01"):
 
     ____
     Parameters:
-        - df_res (pandas.DataFrame, required): reservations DataFrame containing "will_cancel" column
+        - df_res (pandas.DataFrame, required): future-looking reservations DataFrame containing "will_cancel" column
         - as_of_date (str ("%Y-%m-%d"), optional): resulting day-by-days DataFrame will start on this day
         - cxl_type (str, optional): either "a" (actual) or "p" (predicted). Default value is "p".
     """
-    df_dates = df_res.copy()
-    date = pd.to_datetime(first_date, format="%Y-%m-%d")
+    df_dates = df_futures.copy()
+    date = pd.to_datetime(as_of_date, format="%Y-%m-%d")
     end_date = datetime.date(2017, 8, 31)
     delta = datetime.timedelta(days=1)
     max_los = int(df_dates["LOS"].max())
@@ -49,28 +56,33 @@ def setup_sim(df_res, as_of_date="2017-08-01"):
 
             date_stats["RoomsOTB"] += len(df_dates[mask])
             date_stats["RevOTB"] += df_dates[mask].ADR.sum()
+            date_stats["CxlForecast"] += df_dates[mask].will_cancel.sum()
 
             tmp = (
-                df_dates[mask][["ResNum", "CustomerType", "ADR"]]
+                df_dates[mask][["ResNum", "CustomerType", "ADR", "will_cancel"]]
                 .groupby("CustomerType")
-                .agg({"ResNum": "count", "ADR": "sum"})
-                .rename(columns={"ResNum": "RS", "ADR": "Rev"})
+                .agg({"ResNum": "count", "ADR": "sum", "will_cancel": "sum"})
+                .rename(columns={"ResNum": "RS", "ADR": "Rev", "will_cancel": "CXL"})
             )
 
             if "Transient" in list(tmp.index):
                 date_stats["Trn_RoomsOTB"] += tmp.loc["Transient", "RS"]
+                date_stats["Trn_CxlProj"] += tmp.loc["Transient", "CXL"]
                 date_stats["Trn_RevOTB"] += tmp.loc["Transient", "Rev"]
 
             if "Transient-Party" in list(tmp.index):
                 date_stats["TrnP_RoomsOTB"] += tmp.loc["Transient-Party", "RS"]
+                date_stats["TrnP_CxlProj"] += tmp.loc["Transient-Party", "CXL"]
                 date_stats["TrnP_RevOTB"] += tmp.loc["Transient-Party", "Rev"]
 
             if "Group" in list(tmp.index):
                 date_stats["Grp_RoomsOTB"] += tmp.loc["Group", "RS"]
+                date_stats["Grp_CxlProj"] += tmp.loc["Group", "CXL"]
                 date_stats["Grp_RevOTB"] += tmp.loc["Group", "Rev"]
 
             if "Contract" in list(tmp.index):
                 date_stats["Cnt_RoomsOTB"] += tmp.loc["Contract", "RS"]
+                date_stats["Cnt_CxlProj"] += tmp.loc["Contract", "CXL"]
                 date_stats["Cnt_RevOTB"] += tmp.loc["Contract", "Rev"]
 
             tminus += 1
@@ -79,3 +91,64 @@ def setup_sim(df_res, as_of_date="2017-08-01"):
         date += delta
 
     return pd.DataFrame(nightly_stats).transpose()
+
+
+def add_sim_cols(df_sim, capacity):
+    """
+    Adds several columns to df_sim, including:
+        - 'Occ' (occupancy)
+        - 'RevPAR' (revenue per available room)
+        - 'ADR' (by segment)
+        - 'DOW' (day-of-week)
+        - 'WD' (weekday - binary)
+        - 'WE' (weekend - binary)
+        - 'STLY_Date' (datetime "%Y-%m-%d")
+
+    _____
+    Parameters:
+        - df_sim: day-by-day hotel DF
+        - capacity (int, required): number of rooms in the hotel
+    """
+    # add Occ & RevPAR columns'
+    df_sim["Occ"] = round(df_sim["RoomsOTB"] / capacity, 2)
+    df_sim["RevPAR"] = round(df_sim["RevOTB"] / capacity, 2)
+
+    # Add ADR by segment
+    df_sim["ADR_OTB"] = round(df_sim.RevOTB / df_sim.RoomsOTB, 2)
+    df_sim["Trn_ADR_OTB"] = round(df_sim.Trn_RevOTB / df_sim.Trn_RoomsOTB, 2)
+    df_sim["TrnP_ADR_OTB"] = round(df_sim.TrnP_RevOTB / df_sim.TrnP_RoomsOTB, 2)
+    df_sim["Grp_ADR_OTB"] = round(df_sim.Grp_RevOTB / df_sim.Grp_RoomsOTB, 2)
+    df_sim["Cnt_ADR_OTB"] = round(df_sim.Cnt_RevOTB / df_sim.Cnt_RoomsOTB, 2)
+
+    dow = pd.to_datetime(df_sim.index, format="%Y-%m-%d")
+    dow = dow.strftime("%a")
+    df_sim.insert(0, "DOW", dow)
+    df_sim["WE"] = (df_sim.DOW == "Fri") | (df_sim.DOW == "Sat")
+    df_sim["WD"] = df_sim.WE == False
+
+    # add STLY date
+    stly_lambda = lambda x: pd.to_datetime(x) + relativedelta(
+        years=-1, weekday=pd.to_datetime(x).weekday()
+    )
+    df_sim["STLY_Date"] = df_sim.index.map(stly_lambda)
+
+    df_sim.fillna(0, inplace=True)
+
+    return df_sim
+
+
+def add_stly_cols(df_sim, df_dbd):
+    """
+    Adds the following columns to df_sim:
+        - Last year actual: Rooms Sold, ADR, Cancellations
+        - STLY: RoomsOTB, ADR_OTB, TotalRoomsBooked_L30 & L90
+
+    ____
+    Parameters:
+        - df_sim (pandas.DataFrame, required): simulation DataFrame (future looking)
+        - df_dbd (pandas.DataFrame, required): actual hotel-level data for entire dataset
+    """
+    first_date = df_sim.index.min()
+    last_date = df_sim.index.max()
+
+    return
