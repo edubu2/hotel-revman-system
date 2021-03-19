98199d25300a896d4c08206dd3cfb0ca4a2b8b23
Elliot Wilens <wilensel@gmail.com>
2021-03-19 07:26:58 +0000

    Update save_sims.py instructions & rm ss_2

--- code/save_sims.py
+++ code/save_sims.py
@@ -6,18 +6,15 @@ ON-THE-BOOKS DATA SERIALIZATION SCRIPTS - PART 1 OF 2
 INTRODUCTION
 -------
 
-The 'save_sims' ('_1' and '_2') scripts capture future-looking on the books (OTB)
+This script capture future-looking on the books (OTB)
 data as_of every date in our sample that has one year of history and serializes them for
 later ease of use using Pickle. This means the first year of data cannot be used
-directly in the output, but data from that year will be contained in the STLY (same-time-
-last-year) features of the save_sims_2 output. The scripts need to be run in order. I am
-currently experimenting with starting save_sims_2.py once save_sims_1.py is halfway done.
-I will update this when I can confirm it works. It will be extremely CPU intensive and
-should not be attempted on a personal computer (unless you have state of the art hardware).
+directly in the output, but data from that year is still needed for STLY (same-time last year) features.
 
-Hyperthreading should be enabled.
+This script is extremely CPU intensive and should not be attempted on a personal computer
+(unless you have state of the art hardware). Hyperthreading should be enabled.
 
-This script generates nearly 1000 .csv files containing on the books data 
+This script generates nearly 1000 Pickle (.pick) files containing on the books data 
 as of each date from 2015-07-01 to 2017-08-31 for both H1 and H2. 
 
 The subsequent save_sims_2.py script will pull STLY data from 2017-08-01 to
@@ -25,21 +22,10 @@ The subsequent save_sims_2.py script will pull STLY data from 2017-08-01 to
 
 INSTRUCTIONS
 ------
-Upon completion, run save_sims_2.py. save_sims_2.py is nearly identical to this
-script, but passes different parameters into generate_simulation function with each
-iteration.
-
-It takes several hours for each script to run (very CPU intensive). Once the files are saved,
-the data can be accessed and manipulated very quickly.
-
-We need to do this in order to quickly pull STLY OTB data for future simulations.
--------
-
-Upon initial setup, after saving df_res and df_dbd to pickle files *, run this script
+Upon initial setup, after saving df_res and df_dbd to pickle files*, run this script
 from your terminal.
-
-* df_res and df_sim are generated from the 'dbds.py' script. Replace 'df' with 'h<n>';
-replace '<n>' with the hotel number. 
+    - *df_res and df_sim are generated from the 'dbds.py' script. Replace 'df' with 'h<n>';
+      replace '<n>' with the hotel number. 
 
 Ensure the global params are as follows: 
 FOLDER = "./sims/pickle/"
@@ -59,15 +45,9 @@ import datetime
 from sim import generate_simulation
 import time
 
-# --- ADJUST THESE VARIABLES FROM STEP 1 - STEP 2 ---
-# --- SEE INSTRUCTIONS IN DOCSTRING ---
 FOLDER = "./sims/pickle/"
 START = datetime.date(2015, 8, 1)
 STOP = datetime.date(2017, 8, 31)
-PULL_EXTENDED = False  # set to True only for run 2 (see instructions in docstring)
-
-# ---- STOP ----
-
 DATE_FMT = "%Y-%m-%d"
 H1_RES = pd.read_pickle("pickle/h1_res.pick")
 H2_RES = pd.read_pickle("pickle/h2_res.pick")
@@ -85,44 +65,24 @@ def save_sim_records(
     ]
 
     counter = 1
-
     for as_of_date in all_dates:
-
         # if counter % 100 == 0:
-        #     time.sleep(60)  # save cpu
+        #     time.sleep(60)  # save cpu (comment out for VMs)
         out_file = str(FOLDER + f"h{str(hotel_num)}_sim_{as_of_date}.pick")
-
-        if PULL_EXTENDED:
-            df_sim = generate_simulation(
-                df_dbd,
-                as_of_date,
-                hotel_num,
-                df_res,
-                confusion=False,
-                pull_stly=True,
-                verbose=0,
-                pull_lya=True,
-                add_pace=True,
-                add_cxl_realized=True,
-            )
-
-        else:
-            df_sim = generate_simulation(
-                df_dbd,
-                as_of_date,
-                hotel_num,
-                df_res,
-                confusion=False,
-                pull_stly=False,
-                verbose=0,
-                pull_lya=False,
-                add_pace=False,
-                add_cxl_realized=False,
-            )
-
+        df_sim = generate_simulation(
+            df_dbd,
+            as_of_date,
+            hotel_num,
+            df_res,
+            confusion=False,
+            pull_stly=False,
+            verbose=0,
+            pull_lya=False,
+            add_pace=False,
+            add_cxl_realized=False,
+        )
         df_sim.to_pickle(out_file)
         counter += 1
-
         print(f"Saved file {out_file}.")
 
     return
