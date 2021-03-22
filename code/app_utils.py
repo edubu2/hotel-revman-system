14fe14938a678262da1835d039f1a08ace094fab
Elliot Wilens <wilensel@gmail.com>
2021-03-22 03:41:45 +0000

    Add new app_utils that contains display_cols

---
+++ code/app_utils.py
@@ -0,0 +1,13 @@
+display_cols = [
+    "StayDate",
+    "DOW",
+    "SellingPrice",
+    "OptimalPrice",
+    "ProjOcc",
+    "RoomsOTB",
+    "TRN_RoomsOTB",
+    "Pace_RoomsOTB",
+    "Pace_TRN_RoomsOTB",
+    "LYA_Occ",
+    "",
+]
