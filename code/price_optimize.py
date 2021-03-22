f5007b61a42dc3ce70c10657e848de2a3ba166d1
Elliot Wilens <wilensel@gmail.com>
2021-03-22 02:13:12 +0000

    Add working price_optimize.pygit add price_optimize.py !

--- code/price_optimize.py
+++ code/price_optimize.py
@@ -46,21 +46,18 @@ def train_model(df_sim, as_of_date, X_train, y_train, X_test, y_test):
     )
     rfm.fit(X_train, y_train)
     preds = rfm.predict(X_test)
-    print("Finished training model.")
-    r2 = round(r2_score(y_test, preds), 4)
-    print(f"R² Score on test set: {r2}")
 
-    X_test["ACTUAL_TRN_RoomsPickup"] = preds.round(0).astype(int)
-    df_sim["ACTUAL_TRN_RoomsPickup"] = X_test["ACTUAL_TRN_RoomsPickup"]
+    X_test["Proj_TRN_RoomsPickup"] = preds.round(0).astype(int)
+    df_sim["Proj_TRN_RoomsPickup"] = X_test["Proj_TRN_RoomsPickup"]
     mask = df_sim["AsOfDate"] == as_of_date
     df_pricing = df_sim[mask].copy()
     return df_pricing, rfm, preds
 
 
-def calculate_rev_at_price(price, df_sim, model, df_index):
+def calculate_rev_at_price(price, df_pricing, model, df_index):
     """
     Calculates transient room revenue at predicted selling prices."""
-    df = df_sim.copy()
+    df = df_pricing.copy()
     df.loc[df_index, "SellingPrice"] = price
     X = df.loc[df_index, rf_cols].to_numpy()
     trn_rooms_to_book = model.predict([X])[0]
@@ -83,15 +80,25 @@ def get_optimal_prices(df_pricing, as_of_date, model):
         original_rate = round(df_pricing.loc[i, "SellingPrice"], 2)
         date_X = df_pricing.loc[i, rf_cols].to_numpy()
         pred = model.predict([date_X])[0]
-        original_rev = pred * original_rate
-        optimal_rate = (original_rate, original_rev, original_rate, original_rev)
+        proj_rev_old_price = pred * original_rate
+        optimal_rate = (
+            original_rate,
+            proj_rev_old_price,
+            original_rate,
+            proj_rev_old_price,
+        )
 
         for pct in price_adjustments:
             new_rate = round(original_rate * (1 + pct), 2)
-            resulting_rev = calculate_rev_at_price(new_rate, df_sim, model, i)
+            resulting_rev = calculate_rev_at_price(new_rate, df_pricing, model, i)
 
             if resulting_rev > optimal_rate[1]:
-                optimal_rate = (new_rate, resulting_rev, original_rate, original_rev)
+                optimal_rate = (
+                    new_rate,
+                    resulting_rev,
+                    original_rate,
+                    proj_rev_old_price,
+                )
 
             else:
                 continue
@@ -107,17 +114,16 @@ def add_rates(df_pricing, optimal_prices):
     new_rates = []
     resulting_revs = []
     original_rates = []
-    original_revs = []
-    for new_rate, resulting_rev, original_rate, original_rev in optimal_prices:
+    proj_rev_old_prices = []
+    for new_rate, resulting_rev, original_rate, proj_rev_old_price in optimal_prices:
         new_rates.append(new_rate)
         resulting_revs.append(resulting_rev)
         original_rates.append(original_rate)
-        original_revs.append(original_rev)
+        proj_rev_old_prices.append(round(proj_rev_old_price, 2))
 
     df_pricing["OptimalRate"] = new_rates
     df_pricing["TRN_RevPickupAtOptimal"] = resulting_revs
-    df_pricing["SellingRate"] = original_rates
-    df_pricing["TRN_ActualRevPickup"] = original_revs
+    df_pricing["TRN_RevPickupAtOriginal"] = proj_rev_old_prices
 
     return df_pricing
 
@@ -126,13 +132,13 @@ def summarize_model_results(model, y_test, preds):
     """Writes model metrics to STDOUT."""
     r2 = r2_score(y_test, preds)
     mae = mean_absolute_error(y_test, preds)
-    mse = mean_absolute_error(y_test, preds)
+    mse = mean_squared_error(y_test, preds)
 
     print(
-        f"R² score on test set (stay dates Aug 1 - Aug 31, 2017):                        {r2}\n"
+        f"R² score on test set (stay dates Aug 1 - Aug 31, 2017):                        {r2}"
     )
     print(
-        f"MAE (Mean Absolute Error) score on test set (stay dates Aug 1 - Aug 31, 2017): {mae}\n"
+        f"MAE (Mean Absolute Error) score on test set (stay dates Aug 1 - Aug 31, 2017): {mae}"
     )
     print(
         f"MSE (Mean Squared Error) score on test set (stay dates Aug 1 - Aug 31, 2017):  {mse}\n"
@@ -140,18 +146,54 @@ def summarize_model_results(model, y_test, preds):
     pass
 
 
+def add_finishing_touches(df_pricing):
+    """
+    Adds the following columns:
+        - RecommendedPriceChange (optimal rate variance to original rate)
+        - RevOpportunity (projected revenue change at optimal rates)
+        - DOW (day of week)
+    """
+    df_pricing["RecommendedPriceChange"] = (
+        df_pricing["OptimalRate"] - df_pricing["SellingPrice"]
+    )
+    avg_price_change = df_pricing["RecommendedPriceChange"].mean()
+    print(
+        f"Average recommended price change...                                  {avg_price_change}"
+    )
+
+    df_pricing["ProjRevOpportunity"] = (
+        df_pricing["TRN_RevPickupAtOptimal"] - df_pricing["TRN_RevPickupAtOriginal"]
+    )
+    total_rev_opp = df_pricing["ProjRevOpportunity"].sum()
+    print(
+        f"Estimated revenue growth from implementing price recommendations...  {total_rev_opp} "
+    )
+
+    df_pricing["DOW"] = (
+        df_pricing["StayDate"]
+        .map(lambda x: dt.datetime.strftime(x, format="%a"))
+        .astype(str)
+    )
+
+    return df_pricing.copy()
+
+
 def recommend_pricing(df_sim, as_of_date):
 
-    print("Training Random Forest model to predict remaining transient demand...\n")
+    print("Training Random Forest model to predict remaining transient demand...")
     X_train, y_train, X_test, y_test = splits(df_sim, as_of_date)
     df_pricing, model, preds = train_model(
         df_sim, as_of_date, X_train, y_train, X_test, y_test
     )
 
-    print("Model ready. Calculating optimal selling prices...\n")
+    print("Model ready.\n")
+    summarize_model_results(model, y_test, preds)
+
+    print("Calculating optimal selling prices...\n")
     df_pricing, optimal_prices = get_optimal_prices(df_pricing, as_of_date, model)
     df_pricing = add_rates(df_pricing, optimal_prices)
+    df_pricing = add_finishing_touches(df_pricing)
 
     print("Simulation ready.\n")
-    summarize_model_results(model, y_test, preds)
+
     return df_pricing
